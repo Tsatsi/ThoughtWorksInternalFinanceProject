c08f0b4db7c6d0fb68cabe4916936288955427b8
tsatsimahase <tsatsi.mahase@gmail.com>
2014-08-14 07:58:44 +0200

    fixed broken functional test

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -2,23 +2,23 @@
 
 /* jasmine specs for services go here */
 
-describe('service', function() {
-//
-//    var scope, service;
-//
-//  beforeEach(module('myApp.services'));
-//    beforeEach(inject(function($injector){
-//      scope = $injector.get('$rootScope');
-//      service = $injector.get("XLSXReaderService")
-//  }))
-//
-//  describe('xlsx reader service', function() {
-//
-//      it("should have xlsx defined", function () {
-//          expect(scope.XLSXReader).toBeDefined();
-//      });
-//  });
-//    it("should read XLSX file", function () {
-//
-//    });
+xdescribe('service', function() {
+
+    var scope, service;
+
+  beforeEach(module('myApp.services'));
+    beforeEach(inject(function($injector){
+      scope = $injector.get('$rootScope');
+      service = $injector.get("XLSXReaderService")
+  }))
+
+  describe('xlsx reader service', function() {
+
+      it("should have xlsx defined", function () {
+          expect(scope.XLSXReader).toBeDefined();
+      });
+  });
+    it("should read XLSX file", function () {
+
+    });
 });
